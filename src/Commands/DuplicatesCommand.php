0edc6593b8c282c4369586e9a4911dfe7a036a25
Aleksandr Riabov <zkelo@yandex.ru>
2022-12-04 23:22:59 +0300

    Updated code

--- src/Commands/DuplicatesCommand.php
+++ src/Commands/DuplicatesCommand.php
@@ -16,6 +16,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 class DuplicatesCommand extends Command
 {
     /**
+     * Filename column name option
+     */
+    const OPTION_FILEPATH_COLUMN = 'filepath_column';
+
+    /**
+     * Hash column name option
+     */
+    const OPTION_HASH_COLUMN = 'hash';
+
+    /**
      * {@inheritDoc}
      */
     protected static $defaultName = 'duplicates';
@@ -32,8 +42,8 @@ class DuplicatesCommand extends Command
     {
         $this->addArgument('path', InputArgument::REQUIRED, 'Path');
 
-        $this->addOption('filepath_column', 'fpcol', InputOption::VALUE_OPTIONAL, 'Filepath column name', 'filepath');
-        $this->addOption('hash_column', 'hcol', InputOption::VALUE_OPTIONAL, 'Hash column name', 'hash');
+        $this->addOption(static::OPTION_FILEPATH_COLUMN, 'fpcol', InputOption::VALUE_OPTIONAL, 'Filepath column name', 'filepath');
+        $this->addOption(static::OPTION_HASH_COLUMN, 'hcol', InputOption::VALUE_OPTIONAL, 'Hash column name', 'hash');
     }
 
     /**
@@ -42,12 +52,56 @@ class DuplicatesCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filepath = $input->getArgument('path');
-        if (empty($filepath)) {
+        if (!file_exists($filepath)) {
             $output->writeln('<error>You need to specify filepath to CSV file with hashes</>');
-            return Command::FAILURE;
+            return Command::INVALID;
         }
 
-        // TODO Make this command work
+        $files = [
+            fopen($filepath, 'r'),
+            fopen($filepath, 'r')
+        ];
+
+        $filepathColumnName = $input->getOption(static::OPTION_FILEPATH_COLUMN);
+        $hashColumnName = $input->getOption(static::OPTION_HASH_COLUMN);
+
+        $headers = [];
+        $first = [
+            true,
+            true
+        ];
+
+        while ($fRow = fgetcsv($files[0])) {
+            if ($first[0]) {
+                $filepathColumn = array_search($filepathColumnName, $fRow);
+                if ($filepathColumn === false) {
+                    $output->writeln("<error>Unable to find \"{$filepathColumnName}\" in input CSV</>");
+                    return Command::INVALID;
+                }
+
+                $hashColumn = array_search($hashColumnName, $fRow);
+                if ($hashColumn === false) {
+                    $output->writeln("<error>Unable to find \"{$hashColumnName}\" in input CSV</>");
+                    return Command::INVALID;
+                }
+
+                $headers[$filepathColumnName] = $filepathColumn;
+                $headers[$hashColumnName] = $hashColumn;
+
+                $first[0] = false;
+                continue;
+            }
+
+            $fpath = $fRow[$headers[$filepathColumnName]];
+            $hash = $fRow[$headers[$hashColumnName]];
+
+            // TODO
+        }
+
+        foreach ($files as $file) {
+            fclose($file);
+        }
+
         return Command::SUCCESS;
     }
 }
