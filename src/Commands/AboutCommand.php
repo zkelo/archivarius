a7dc3f6cb13c3af51ca5328aaeb92074ac6b7de3
Aleksandr Riabov <zkelo@yandex.ru>
2022-12-04 16:02:43 +0300

    Updated `about` command

--- src/Commands/AboutCommand.php
+++ src/Commands/AboutCommand.php
@@ -34,15 +34,14 @@ class AboutCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('You\'re running an <info>' . $this->getApplication()->getName() . ' ' . $this->getApplication()->getVersion() . '</>');
+        $output->writeln('<question><3</> You\'re running an <info>' . $this->getApplication()->getName() . ' ' . $this->getApplication()->getVersion() . '</>');
         $output->writeln('<comment>App that helps you to work with your family photo and video archive.</>');
 
         $output->writeln(['', '<question>???</> <info>What this app can do?</>']);
         $output->writeln('1. Looking for same photos and videos by hashing each with sha1 algorithm;');
         $output->writeln('2. With 1 step, remove duplicates from your archive (with or without backing it up).');
 
-        $output->writeln(['', '<question>***</>']);
-        $output->writeln('Copyright &copy; ' . date('Y') . ' <href=https://zkelo.ru>ZKelo</>');
+        $output->writeln(['', '<question>***</> Copyright &copy; ' . date('Y') . ' <href=https://zkelo.ru>ZKelo</>']);
 
         return Command::SUCCESS;
     }
