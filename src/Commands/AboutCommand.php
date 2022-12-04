1c9ee6f120ba7ff31359b5f95bbd33de52ca1b2c
Aleksandr Riabov <zkelo@yandex.ru>
2022-12-04 15:51:44 +0300

    Updated `about` command

--- src/Commands/AboutCommand.php
+++ src/Commands/AboutCommand.php
@@ -34,8 +34,13 @@ class AboutCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('You\'re running an ' . $this->getApplication()->getName() . ' ' . $this->getApplication()->getVersion());
-        $output->writeln('App that helps you to work with your family photo and video archive.');
+        $output->writeln('You\'re running an <info>' . $this->getApplication()->getName() . ' ' . $this->getApplication()->getVersion() . '</>');
+        $output->writeln('<comment>App that helps you to work with your family photo and video archive.</>');
+
+        $output->writeln(['', '<question>???</> <info>What this app can do?</>']);
+        $output->writeln('1. Looking for same photos and videos by hashing each with sha1 algorithm;');
+        $output->writeln('2. With 1 step, remove duplicates from your archive (with or without backing it up).');
+
         return Command::SUCCESS;
     }
 }
